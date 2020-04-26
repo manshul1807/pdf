75b11f48b789c206c2188d2945eab4fb98d59bd2
manshul1807 <arora.manshul@gmail.com>
2020-04-26 13:41:19 +0200

    third commit

---
+++ watermark.py
@@ -0,0 +1,26 @@
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import sys
+
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+inputs = sys.argv[1:]
+
+
+def create_watermark(input_pdf, output, watermark):
+    watermark_obj = PdfFileReader(watermark)
+    watermark_page = watermark_obj.getPage(0)
+
+    pdf_reader = PdfFileReader(input_pdf)
+    pdf_writer = PdfFileWriter()
+
+    # Watermark all the pages
+    for page in range(pdf_reader.getNumPages()):
+        page = pdf_reader.getPage(page)
+        page.mergePage(watermark_page)
+        pdf_writer.addPage(page)
+
+    with open(output, 'wb') as out:
+        pdf_writer.write(out)
+
+
+create_watermark(inputs[0], inputs[1], inputs[2])
